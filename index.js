df973a600b25af1fe0b08738f3a68eb969f59779
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2025-06-10 15:14:42 +0200

    Made chat work

--- index.js
+++ index.js
@@ -6,6 +6,125 @@ socket = io(socketString, {
     transports: ['websocket'],
 });
 
+var optiflowzChat = document.createElement("div");
+optiflowzChat.id = "optiflowz-chat";
+optiflowzChat.innerHTML = `
+<button id="optiflowz-chat-open">
+    <svg viewBox="0 0 24 24"><path fill="currentColor" d="M12 3c5.5 0 10 3.58 10 8s-4.5 8-10 8c-1.24 0-2.43-.18-3.53-.5C5.55 21 2 21 2 21c2.33-2.33 2.7-3.9 2.75-4.5C3.05 15.07 2 13.13 2 11c0-4.42 4.5-8 10-8"></path></svg>
+    <svg viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6z"></path></svg>
+</button>
+<div class="optiflowz-chat-wrapper" style="display: none;">
+    <section class="optiflowz-chat-header">
+        <div>
+            <img src="aiAgentImg.png" alt="Default Agent Avatar">
+            <h1>AI AGENT</h1>
+        </div>
+        <div>
+            <button id="optiflowz-chat-request-agent">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18.5051 19H20C21.1046 19 22.0669 18.076 21.716 17.0286C21.1812 15.4325 19.8656 14.4672 17.5527 14.1329M14.5001 10.8645C14.7911 10.9565 15.1244 11 15.5 11C17.1667 11 18 10.1429 18 8C18 5.85714 17.1667 5 15.5 5C15.1244 5 14.7911 5.04354 14.5001 5.13552M9.5 14C13.1135 14 15.0395 15.0095 15.716 17.0286C16.0669 18.076 15.1046 19 14 19H5C3.89543 19 2.93311 18.076 3.28401 17.0286C3.96047 15.0095 5.88655 14 9.5 14ZM9.5 11C11.1667 11 12 10.1429 12 8C12 5.85714 11.1667 5 9.5 5C7.83333 5 7 5.85714 7 8C7 10.1429 7.83333 11 9.5 11Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+            <button id="optiflowz-chat-more">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6 13C6.55 13 7 12.55 7 12C7 11.45 6.55 11 6 11C5.45 11 5 11.45 5 12C5 12.55 5.45 13 6 13Z" stroke="black" stroke-width="2"/>
+                    <path d="M12 13C12.55 13 13 12.55 13 12C13 11.45 12.55 11 12 11C11.45 11 11 11.45 11 12C11 12.55 11.45 13 12 13Z" stroke="black" stroke-width="2"/>
+                    <path d="M18 13C18.55 13 19 12.55 19 12C19 11.45 18.55 11 18 11C17.45 11 17 11.45 17 12C17 12.55 17.45 13 18 13Z" stroke="black" stroke-width="2"/>
+                </svg>
+                <div>
+                    <p id="optiflowz-chat-new-chat">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 4V20M20 12H4" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        Nova konverzacija
+                    </p>
+                    <p id="optiflowz-chat-rejoin">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19 13C19 16.866 15.866 20 12 20C8.13401 20 5 16.866 5 13C5 9.13401 8.13401 6 12 6H17M17 6L14 3M17 6L14 9" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        Ponovo se pridruži
+                    </p>
+                    <p id="optiflowz-chat-rating">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.0767 4.21969C11.4183 3.39839 12.5817 3.39839 12.9233 4.21968L14.7811 8.68645L19.6034 9.07305C20.49 9.14413 20.8496 10.2506 20.174 10.8293L16.5 13.9765L17.6225 18.6822C17.8288 19.5474 16.8876 20.2313 16.1285 19.7676L12 17.246L7.87146 19.7676C7.11236 20.2313 6.17111 19.5474 6.3775 18.6822L7.49998 13.9765L3.82593 10.8293C3.1504 10.2506 3.50992 9.14413 4.39658 9.07305L9.21882 8.68645L11.0767 4.21969Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        Oceni konverzaciju
+                    </p>
+                </div>
+            </button>
+        </div>
+    </section>
+    <section class="optiflowz-chat-body">
+        <div class="optiflowz-chat-messages"></div>
+    </section>
+    <section class="optiflowz-chat-input">
+        <div>
+            <textarea name="chat-input" id="optiflowz-chat-textarea" placeholder="Unesite poruku..."></textarea>
+            <button id="optiflowz-chat-send">
+                <svg data-v-2a7fb1c3="" viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="m2 21l21-9L2 3v7l15 2l-15 2z"></path></svg>
+            </button>
+        </div>
+    </section>
+    <div class="optiflowz-rating-wrapper">
+        <div class="closeRatingWrapper" onclick="closeOptiFlowzRatingScreen()"></div>
+        <div class="optiflowz-rating-content">
+            <h2>Kako ste zadovoljni ovom konverzacijom?</h2>
+            <div class="optiflowz-rating-buttons">
+                <button data-rating="1" onclick="setSelected(this, this.parentElement)">1</button>
+                <button data-rating="2" onclick="setSelected(this, this.parentElement)">2</button>
+                <button data-rating="3" onclick="setSelected(this, this.parentElement)">3</button>
+                <button data-rating="4" onclick="setSelected(this, this.parentElement)">4</button>
+                <button data-rating="5" class="selected" onclick="setSelected(this, this.parentElement)">5</button>
+            </div>
+            <div class="optiflowz-rating-actions">
+                <button onclick="closeOptiFlowzRatingScreen()">Otkaži</button>
+                <button id="optiflowz-chat-rate-button">Potrvdi</button>
+            </div>
+        </div>
+    </div>
+    <div class="optiflowz-chat-form">
+        <div class="closeRatingWrapper" onclick="closeOptiFlowzAgentForm()"></div>
+        <div class="optiflowz-rating-content">
+            <div>
+                <p>Ispunite formular kako bismo vas povezali sa našim agentom</p>
+                <input type="text" placeholder="Ime i prezime" class="optiflowz-chat-form-name" required>
+                <input type="email" placeholder="Email adresa" class="optiflowz-chat-form-email" required>
+                <section>
+                    <button onclick="closeOptiFlowzAgentForm()">Otkaži</button>
+                    <button id="optiflowz-chat-request-agent-button">Potrvdi</button>
+                </section>
+            </div>
+        </div>
+    </div>
+    <div class="optiflowz-chat-form-rejoin">
+        <div class="closeRatingWrapper" onclick="closeOptiFlowzRejoinForm()"></div>
+        <div class="optiflowz-rating-content">
+            <div>
+                <p>Unesite kod kako biste se vratili na staru konverzaciju</p>
+                <input type="text" placeholder="Kod" class="optiflowz-chat-rejoin-code" required>
+                <section>
+                    <button onclick="closeOptiFlowzRejoinForm()">Otkaži</button>
+                    <button id="optiflowz-chat-rejoin-button">Potrvdi</button>
+                </section>
+            </div>
+        </div>
+    </div>
+    <div class="optiflowz-chat-error">
+        <div class="closeRatingWrapper" onclick="closeOptiFlowzErrorPopup()"></div>
+        <div class="optiflowz-rating-content">
+            <div>
+                <h2>Desila se greska!</h2>
+                <section>
+                    <button onclick="closeOptiFlowzErrorPopup()">Zatvori</button>
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
+`;
+document.body.appendChild(optiflowzChat);
+document.body.innerHTML += `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/OptiFlowz/Laptop-Centar-Chat/style.css">`;
+
 // Uspostavljanje konekcije sa soket serverom
 socket.once("connect", async () => {
     window.socket = socket;
